4efe8e99d5074772b276db0b009081a977517837
Ivan <morozecc1995@gmail.com>
2020-11-02 14:21:18 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+
+ let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+ };
+ console.log(persone.name, persone.age);
+
+ // alert("Hello world!");
+  
+ //let answer = confirm("Are you here?");
+ //console.log(answer);
+
+ let answer = prompt ("What is your name?", "");
+ console.log(answer);
+
+ let incr = 10,
+     decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
