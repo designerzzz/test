79824819e355a50be54eaa5f92db33da59bb41f5
designerzzz <73472026+designerzzz@users.noreply.github.com>
2020-11-05 17:22:24 +0300

    Урок 2
    123

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц",""),
+let money = +prompt("Ваш бюджет на месяц",""),
     time = prompt("Введите дату в формате YYYY-MM-DD");
 
     let appData = {
@@ -11,9 +11,11 @@ let money = prompt("Ваш бюджет на месяц",""),
         timeData:time,
         savings:false
     };
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-    b = prompt("Во сколько обойдется?", '');
-
+    
+    
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+    
     if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
         console.log("done");
       
@@ -24,3 +26,25 @@ let money = prompt("Ваш бюджет на месяц",""),
 appData.moneyPerDay = appData.budget / 30;
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
+   if (appData.moneyPerDay < 100){
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000){
+        console.log("Высокий уровень достатка");
+    } else {
+       console.log("Произошла ошибка");
+    }
+    //while (appData.moneyPerDay < 100){
+      //  console.log("Минимальный уровень достатка");
+    //}
+    //while (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+     //   console.log("Средний уровень достатка");
+    //}
+    //while (appData.moneyPerDay > 2000){
+     //   console.log("Высокий уровень достатка");
+    //}
+    //do {
+      //  console.log("Минимальный уровень достатка")
+    //}
+//    while (appData.moneyPerDay < 100);
