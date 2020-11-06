cb2b370601c11014b76c5e1c7a35522fd7712c4c
designerzzz <73472026+designerzzz@users.noreply.github.com>
2020-11-06 12:03:44 +0300

    urok2 dop
    urok2 dop

--- script.js
+++ script.js
@@ -1,50 +1,19 @@
 'use strict';
-
-let money = +prompt("Ваш бюджет на месяц",""),
-    time = prompt("Введите дату в формате YYYY-MM-DD");
-
-    let appData = {
-        budget:money,
-        expenses:{},
-        optionalExpenses:{},
-        income:[],
-        timeData:time,
-        savings:false
-    };
-    
-    
-        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-            b = prompt("Во сколько обойдется?", '');
-    
-    if ( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-      
-        appData.expenses[a] = b;
-} else {
-
-}
-appData.moneyPerDay = appData.budget / 30;
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-   if (appData.moneyPerDay < 100){
-        console.log("Минимальный уровень достатка");
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-        console.log("Средний уровень достатка");
-    } else if (appData.moneyPerDay > 2000){
-        console.log("Высокий уровень достатка");
-    } else {
-       console.log("Произошла ошибка");
+let week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+week.forEach((item) => {
+    if (item == ('суббота') || item == ('воскресенье')) {
+     document.write(item.bold(),"<br>");
+    } else if (item == ('пятница')) {
+            document.write(item.italics(),"<br>");}
+    else {
+        document.write(item,"<br>");
     }
-    //while (appData.moneyPerDay < 100){
-      //  console.log("Минимальный уровень достатка");
-    //}
-    //while (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
-     //   console.log("Средний уровень достатка");
-    //}
-    //while (appData.moneyPerDay > 2000){
-     //   console.log("Высокий уровень достатка");
-    //}
-    //do {
-      //  console.log("Минимальный уровень достатка")
-    //}
-//    while (appData.moneyPerDay < 100);
+  });
+
+let arr = ['32', '27', '555', '104', '47', '88', '377']; 
+
+arr.forEach((item) => {
+  if (item.startsWith('3') || item.startsWith('7')) {
+   document.write(item,"<br>");
+  }
+});
